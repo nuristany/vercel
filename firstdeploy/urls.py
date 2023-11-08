0d294641491126f32ecb70a859d2381a0d091ac8
Qais <nuristanyqais@gmail.com>
2023-11-08 23:10:57 +0430

    Updated URL patterns in firstdeploy/urls.py

--- firstdeploy/urls.py
+++ firstdeploy/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from firstdeploy.firstdeploy import settings
+from firstdeploy import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
